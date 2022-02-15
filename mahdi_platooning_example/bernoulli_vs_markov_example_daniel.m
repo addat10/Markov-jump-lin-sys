d4867086e1300097c97e9dec5be8a554e1507e42
Adwait Datar <datar.adwait@gmail.com>
2022-02-15 12:48:01 +0100

    Remove bug(normalize station. distrib) and include leaderless consensus

--- mahdi_platooning_example/bernoulli_vs_markov_example_daniel.m
+++ mahdi_platooning_example/bernoulli_vs_markov_example_daniel.m
@@ -15,8 +15,9 @@ Kp=0.45:0.01:0.7;
 T=1;
 rho_T_MJLS=zeros(length(Kp),1);
 rho_T_Ber=zeros(length(Kp),1);
+lead_flag=0;% Set to zero for consensus problem and 1 if leader present
 for i=1:length(Kp)
-[MJLS,Ber]=example_ber_vs_markov_first_order_consensus_mahdi_data(TPM_path,Kp(i),T);  
+[MJLS,Ber]=example_ber_vs_markov_first_order_consensus_mahdi_data(TPM_path,Kp(i),T,lead_flag);  
 % Analyze the defined MJLS
 [B_cal_MJLS,T_cal_MJLS]=get_B_T_matrices(MJLS);
 [B_cal_Ber,T_cal_Ber]=get_B_T_matrices(Ber);
@@ -42,7 +43,7 @@ title('Spectrum of the transition Matrix')
 
 
 %% helper functions
-function [MJLS,Ber]=example_ber_vs_markov_first_order_consensus_mahdi_data(TPM_path,Kp,T)
+function [MJLS,Ber]=example_ber_vs_markov_first_order_consensus_mahdi_data(TPM_path,Kp,T,lead_flag)
 % Create an MJLS system with data from Mahdi
 MJLS.N=64;
 % Need to create 64 A matrices
@@ -56,10 +57,13 @@ At=diag(ones(n,1))+diag(ones(n-1,1),1); % Always connected
 for i=1:MJLS.N            
     At=update_adjacency(At,i);
     D=diag(At'*ones(n));
-    D(1)=2;                             % Leader externally controlled
+    if lead_flag==1
+        % Don't need the leader for the consensus problem
+        D(1)=2;
+    end
     Li=diag(D)-At';
     MJLS.Laps{i}=Li;
-    [Ai,Bi]=get_AB(Li,Kp,MJLS.dt);
+    [Ai,Bi]=get_AB(Li,Kp,MJLS.dt,lead_flag);
     MJLS.As{i}=Ai;
     MJLS.Bs{i}=Bi;
 end
@@ -72,14 +76,13 @@ MJLS.T=Transition_Probability;
 
 % Find the stationary distribution
 [eig_vecs,~] = eigs(Transition_Probability);
-p_eqm=eig_vecs(:,1);
-%Genetare a random initial distribution
+p_eqm=eig_vecs(:,1)/sum(eig_vecs(:,1)); % Normalize it so that the sum is 1
+%Genetare a random initial distribution but is required only for simulation
 p_ic=rand(64,1);
 MJLS.p_ic=1/sum(p_ic)*p_ic;
 
 Ber=MJLS;
 Ber.T=p_eqm*ones(1,size(p_eqm,1),1);
-
 end
 function At=update_adjacency(At,i)
     vec=dec2bin(i-1,6);
@@ -90,8 +93,14 @@ function At=update_adjacency(At,i)
     At(2,5)=bin2dec(vec(5));
     At(3,5)=bin2dec(vec(6));
 end
-function [A,B]=get_AB(L,Kp,T)
+function [A,B]=get_AB(L,Kp,T,lead_flag)
 n=size(L,1);
-A=eye(n)-Kp*T*L;
+if lead_flag==0
+    % Need the projection for the consensus system (marginally stable)
+    Proj=(eye(n)-ones(n)/n);
+    A=Proj*(eye(n)-Kp*T*L);
+else
+    A=eye(n)-Kp*T*L;
+end
 B=T*Kp*L;
 end
